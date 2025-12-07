bac04faba64dde9a1d3e58be2379098829f3946e
marama03 <chiteji@gmail.com>
2025-12-07 14:55:33 -0500

    Fix CORS - add DELETE method to allowed methods

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ async function searchWeaviate(embedding, limit = 5) {
 
 const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
